1e3f4866b2f1bc731e6ec3fcc6ad8cb9732506ef
Akash Roy <royaku6@gmail.com>
2023-06-19 02:22:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const colors = [
+    '#ff0000',
+    '#00ff00',
+    '#0000ff',
+    '#ff8000',
+    '#8000ff',
+    '#00ffff',
+    '#ffff00',
+    '#ff0080',
+    '#0080ff',
+    '#80ff00',
+    '#ff00ff',
+    '#008080',
+    '#808000',
+    '#800080',
+    '#ff80ff',
+    '#80ffff',
+    '#ffff80',
+    '#800000',
+    '#008000',
+    '#000080'
+  ];
+  const [showColors, setShowColors] = useState(false);
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const handleButtonClick = () => {
+    setShowColors(!showColors);
+  };
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
+    setShowColors(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={handleButtonClick} className='button'>Pick a color</button>
+      {showColors && (
+        <div>
+          {colors.map((color, index) => (
+            <div
+              key={index}
+              style={{
+                backgroundColor: color,
+                width: '30px',
+                height: '30px',
+                display: 'inline-block',
+                margin: '5px',
+                cursor: 'pointer',
+              }}
+              onClick={() => handleColorClick(color)}
+            ></div>
+          ))}
+        </div>
+      )}
+      {selectedColor && <p>You picked: {selectedColor}</p>}
     </div>
   );
-}
+};
 
 export default App;
